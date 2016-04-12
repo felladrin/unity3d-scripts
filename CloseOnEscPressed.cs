f73eca8186e51c95508882e111e62b2b0e42e7bb
Victor Nogueira <felladrin@gmail.com>
2016-04-11 22:43:11 -0300

    Add CloseOnEscPressed.cs

---
+++ CloseOnEscPressed.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace HeadOfState
+{
+	public class CloseOnEscPressed : MonoBehaviour
+	{
+		void Update ()
+		{
+			if (gameObject.activeInHierarchy && Input.GetKey (KeyCode.Escape))
+				gameObject.SetActive (false);
+		}
+	}
+}
