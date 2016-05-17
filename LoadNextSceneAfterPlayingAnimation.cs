ff8265be9d7ae3d92981c81041c4f6300da354ad
Victor Nogueira <felladrin@gmail.com>
2016-05-17 00:52:45 -0300

    Add LoadNextSceneAfterPlayingAnimation.cs

---
+++ LoadNextSceneAfterPlayingAnimation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadNextSceneAfterPlayingAnimation : MonoBehaviour
+{
+	public int AnimationClipIndex = 0;
+
+	IEnumerator Start ()
+	{
+		Animator animator = GetComponent<Animator> ();
+		yield return new WaitForSeconds (animator.runtimeAnimatorController.animationClips [AnimationClipIndex].length);
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
