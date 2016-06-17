c63c0318f8809a696fb627c3a9c808e6342c2bb9
Victor Nogueira <felladrin@gmail.com>
2016-06-16 23:52:58 -0300

    Add ToggleImageOnClick.cs

---
+++ ToggleImageOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleImageOnClick : MonoBehaviour {
+	public Sprite DefaultImage;
+	public Sprite AlternativaImage;
+
+	private Image imageComponent;
+
+	// Use this for initialization
+	void Start () {
+		imageComponent = GetComponent<Image>();
+	}
+
+	void OnClick() {
+		imageComponent.sprite = (imageComponent.sprite == DefaultImage) ? AlternativaImage : DefaultImage;
+	}
+}
