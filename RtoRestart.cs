7cce789975fdb6e5cfa28e045c549d7eff328779
Victor Nogueira <felladrin@gmail.com>
2016-11-19 21:28:57 -0300

    Add script RtoRestart.cs

---
+++ RtoRestart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RtoRestart : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
