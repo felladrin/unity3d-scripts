ea47f9a3d0b291aae59a3ce3f41ca1763762d5b6
Victor Nogueira <felladrin@gmail.com>
2017-10-09 22:50:07 -0300

    Add ToggleBehaviourEnabledOnGamePaused.cs

---
+++ ToggleBehaviourEnabledOnGamePaused.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ToggleBehaviourEnabledOnGamePaused : MonoBehaviour
+{
+	public Behaviour behaviour;
+
+	void OnGamePaused (bool paused)
+	{
+		behaviour.enabled = !paused;
+	}
+
+	void OnEnable ()
+	{
+		EventManager.OnGamePaused += OnGamePaused;
+	}
+
+	void OnDisable ()
+	{
+		EventManager.OnGamePaused -= OnGamePaused;
+	}
+}
