6a4fe3831e7be3233378b06cc8ba59eb6f63dd2a
Victor Nogueira <felladrin@gmail.com>
2017-10-09 23:01:06 -0300

    Add MoveVerticallyWithKeys.cs

---
+++ MoveVerticallyWithKeys.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MoveVerticallyWithKeys : MonoBehaviour
+{
+	public float Velocity;
+	public Rigidbody2D rigidBody;
+	public KeyCode upKey;
+	public KeyCode downKey;
+
+	void Update ()
+	{
+		if (Input.GetKey (upKey)) {
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, Velocity);
+		} else if (Input.GetKey (downKey)) {
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, -Velocity);
+		} else {
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, 0);
+		}
+	}
+}
