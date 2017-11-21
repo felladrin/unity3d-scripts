bc2ef2cb5abb7ebcb2020f6f5191cd3b919db0c7
Victor Nogueira <felladrin@gmail.com>
2017-11-21 11:15:00 -0300

    Add RandomColorOnStart.cs

---
+++ RandomColorOnStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RandomColorOnStart : MonoBehaviour
+{
+	public float hueMin = 0.1f;
+	public float hueMax = 1f;
+	public float saturationMin = 1f;
+	public float saturationMax = 1f;
+	public float valueMin = 0.5f;
+	public float valueMax = 1f;
+
+	void Start ()
+	{
+		GetComponent<Renderer> ().material.color = Random.ColorHSV (hueMin, hueMax, saturationMin, saturationMax, valueMin, valueMax);
+	}
+}
