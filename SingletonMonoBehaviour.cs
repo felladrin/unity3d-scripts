f4e7509168a86c04faf2e22eee93fd8a8975af9b
Victor Nogueira <felladrin@gmail.com>
2018-02-12 11:26:21 -0300

    Add SingletonMonoBehaviour.cs

---
+++ SingletonMonoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : Component
+{
+    public bool dontDestroyOnLoad;
+
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance != null) return instance;
+            instance = FindObjectOfType<T>();
+            return instance != null ? instance : new GameObject {name = typeof(T).Name}.AddComponent<T>();
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this as T;
+
+        if (dontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
